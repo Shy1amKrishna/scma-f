10ab2e0e3dcafd6203faa26ba279e484e3481fdb
say00j <sayoojsayana123@gmail.com>
2024-01-24 20:53:38 +0530

    update
    added home page

---
+++ frontend/src/Components/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Home.css' 
+import { useNavigate } from 'react-router-dom';
+export const Home = () => {
+
+  const navigate = useNavigate();
+
+  const handleDivClick = (path) => {
+    navigate(path);
+  };
+
+  return (
+    <>
+      <div className='container1'>
+        <div className='container2'>
+        <h1 className="cool-heading">SCMA</h1>
+        <div className="underline"></div>
+      <div className='inputs'>
+        <div className='input' onClick={() => handleDivClick('/home/compilerLab')}><h2>Compiler Lab</h2></div>
+        <div className='input' onClick={() => handleDivClick('/home/programmingLab')}><h2>Programming Lab</h2></div>
+        <div className='input' onClick={() => handleDivClick('/home/softwareLab')}><h2>Software Lab</h2></div>
+      </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
