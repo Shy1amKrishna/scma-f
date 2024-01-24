4f4850ebb1019335f23500412ac8b91639992df2
say00j <sayoojsayana123@gmail.com>
2024-01-24 21:02:49 +0530

    Assets added
    I added necessary assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+      <Route path="/" element={<LoginSignup/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/home/compilerLab" element={<CompilerLab/>}/>
+      <Route path="/home/softwareLab" element={<SoftwareLab/>}/>
+      <Route path="/home/programmingLab" element={<ProgrammingLab/>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Routes>
     </div>
   );
 }
