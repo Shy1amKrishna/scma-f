54d5ee8f2d686e8732cebf7f7595a262ae35286f
say00j <sayoojsayana123@gmail.com>
2024-01-25 15:32:22 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import './App.css';
-import {Home}  from './Components/Home/Home';
-import {LoginSignup}  from './Components/LoginSignup/LoginSignup';
-import {NotFoundPage}  from './Components/NotFoundPage/NotFoundPage';
-import { CompilerLab } from './Components/Compiler Lab/CompilerLab';
-import { SoftwareLab } from './Components/Software Lab/SoftwareLab'
-import { ProgrammingLab } from './Components/Programming Lab/ProgrammingLab';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Home } from "./Components/Home/Home";
+import { LoginSignup } from "./Components/LoginSignup/LoginSignup";
+import { NotFoundPage } from "./Components/NotFoundPage/NotFoundPage";
+import { CompilerLab } from "./Components/Compiler Lab/CompilerLab";
+import { SoftwareLab } from "./Components/Software Lab/SoftwareLab";
+import { ProgrammingLab } from "./Components/Programming Lab/ProgrammingLab";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/" element={<LoginSignup/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/home/compilerLab" element={<CompilerLab/>}/>
-      <Route path="/home/softwareLab" element={<SoftwareLab/>}/>
-      <Route path="/home/programmingLab" element={<ProgrammingLab/>}/>
-      <Route path="*" element={<NotFoundPage/>}/>
-    </Routes>
+        <Route path="/" element={<LoginSignup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/home/compilerLab" element={<CompilerLab />} />
+        <Route path="/home/softwareLab" element={<SoftwareLab />} />
+        <Route path="/home/programmingLab" element={<ProgrammingLab />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
