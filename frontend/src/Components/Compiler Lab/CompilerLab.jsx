1560cb773bac517c07776d9c4dcd5e5ce120e417
say00j <sayoojsayana123@gmail.com>
2024-01-26 17:32:35 +0530

    Update
    Added CSS to Compiler Lab.

--- frontend/src/Components/Compiler Lab/CompilerLab.jsx
+++ frontend/src/Components/Compiler Lab/CompilerLab.jsx
@@ -8,17 +8,17 @@ export const CompilerLab = () => {
         <h1 className="cool-heading">Compiler Lab</h1>
         <div className="underline"></div>
         <h2>The datalist Element</h2>
-        <div className='inputs'>
+        <div className='Cinputs'>
         <form action="/action_page.php">
-        <input className='input' list="browsers" name="browser" placeholder='Enter system name:'/>
-        <datalist id="browsers">
-        <option value="Edge"/>
-        <option value="Firefox"/>
-        <option value="Chrome"/>
-        <option value="Opera"/>
-        <option value="Safari"/>
+        <input className='Cinput' list="Systems" name="Systems" placeholder='Enter system name:'/>
+        <datalist id="Systems">
+        <option value="System1"/>
+        <option value="System2"/>
+        <option value="System3"/>
+        <option value="System4"/>
+        <option value="System5"/>
         </datalist>
-        <input type="submit"/>
+        <input className='Cinputs' type="submit"/>
         </form>
         </div>
         </div>
