56bf7b87f4a994651d69fd9778a1990d2204bff2
say00j <sayoojsayana123@gmail.com>
2024-01-26 13:33:45 +0530

    Update CompilerLab.jsx
    Modified the heading

--- frontend/src/Components/Compiler Lab/CompilerLab.jsx
+++ frontend/src/Components/Compiler Lab/CompilerLab.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const CompilerLab = () => {
   return (
-    <div>Compiler_Lab</div>
+    <>
+    <div className='container1'>
+        <div className='container2'>
+        <h1 className="cool-heading">Compiler Lab</h1>
+        <div className="underline"></div>
+        </div>
+    </div>
+    </>
   )
 }
