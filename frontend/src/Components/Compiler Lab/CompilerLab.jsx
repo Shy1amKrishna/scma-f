16abdaf3df20ab9666f4925a5c605487c1d7351a
say00j <sayoojsayana123@gmail.com>
2024-01-26 14:16:29 +0530

    Update CompilerLab.jsx
    Dropdown input added.

--- frontend/src/Components/Compiler Lab/CompilerLab.jsx
+++ frontend/src/Components/Compiler Lab/CompilerLab.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './CompilerLab.css'
 export const CompilerLab = () => {
   return (
     <>
@@ -7,6 +7,20 @@ export const CompilerLab = () => {
         <div className='container2'>
         <h1 className="cool-heading">Compiler Lab</h1>
         <div className="underline"></div>
+        <h2>The datalist Element</h2>
+        <div className='inputs'>
+        <form action="/action_page.php">
+        <input className='input' list="browsers" name="browser" placeholder='Enter system name:'/>
+        <datalist id="browsers">
+        <option value="Edge"/>
+        <option value="Firefox"/>
+        <option value="Chrome"/>
+        <option value="Opera"/>
+        <option value="Safari"/>
+        </datalist>
+        <input type="submit"/>
+        </form>
+        </div>
         </div>
     </div>
     </>
