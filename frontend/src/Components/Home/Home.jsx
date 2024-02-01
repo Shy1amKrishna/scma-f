4fa28e90923cd1c8206a8b4f84e89af89587d4c3
say00j <sayoojsayana123@gmail.com>
2024-02-01 23:09:25 +0530

    home responsive
    made home responsive using Chat GPT.

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -11,6 +11,12 @@ export const Home = () => {
 
   return (
     <>
+      <div className="topnav">
+  <a className="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+      </div>
       <div className='container1'>
         <div className='container2'>
         <h1 className="cool-heading">SCMA</h1>
